939586b302b715fa6798950c2c5a5d794b38e11f
Dell-User <Dell-User@DESKTOP-BSDRSH6>
2023-10-17 06:51:41 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expected_input_and_output
+{
+    internal class Program
+    { 
+            public static int HowManyOccurrences(string mainString, string substring)
+        {
+            Console.WriteLine("Method HowManyOccurrences Started");
+            Console.WriteLine("------------------------------");
+            int count = 0;
+            int index = mainString.IndexOf(substring);
+
+            while (index != -1)
+            {
+                count++;
+                index = mainString.IndexOf(substring, index + 1);
+            }
+
+            Console.WriteLine("Method HowManyOccurrences Ended");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine();
+            return count;
+        }
+    }
+}
